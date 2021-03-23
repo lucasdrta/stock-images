ef1d36fba35c05db4a5607b30c062124755cb1be
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-23 10:14:05 -0300

    Create Feed Screen

---
+++ src/screens/Feed/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Header from "../../components/Header";
+
+export default function Feed() {
+  return (
+    <>
+      <Header />
+      <View>
+        <Text>Feed</Text>
+      </View>
+    </>
+  );
+}

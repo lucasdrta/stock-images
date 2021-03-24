e7cafb4752dcd1dcb1a3cff180eb67e4a74f80e8
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-23 21:42:10 -0300

    Create Feed Layout

---
+++ src/screens/Feed/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  photo: {
+    height: 200,
+    backgroundColor: "#000",
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#40494E",
+    flexGrow: 1,
+    margin: 4,
+  },
+});

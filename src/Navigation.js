14924d0f066dd88703d0d646de7c9a0e6454ed63
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-23 23:29:55 -0300

    Add Photo page

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Feed from "./screens/Feed";
 
 const Stack = createStackNavigator();
+
+import Feed from "./screens/Feed";
+import Photo from "./screens/Photo";
 
 export default function Navigation() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Feed" component={Feed} />
+      <Stack.Screen name="Photo" component={Photo} />
     </Stack.Navigator>
   );
 }

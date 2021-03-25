3bdb425dd98f95b88a979e7919873d98d586d94c
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-25 15:22:54 -0300

    Add ImageProvider for use context

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { StatusBar } from "react-native";
 import App from "./src";
+import { ImageProvider } from "./src/contexts/ImageContext";
 
 export default function Main() {
   return (
-    <>
+    <ImageProvider>
       <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
       <App />
-    </>
+    </ImageProvider>
   );
 }

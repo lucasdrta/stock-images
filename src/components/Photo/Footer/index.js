06eaf8fba4db71f36906e118887a6c581393c204
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-25 22:27:54 -0300

    Add Profile information

--- src/components/Photo/Footer/index.js
+++ src/components/Photo/Footer/index.js
@@ -1,18 +1,33 @@
 import React from "react";
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, Image, Text } from "react-native";
+import LazyImage from "../../LazyImage";
 import { Feather } from "@expo/vector-icons";
 
 import styles from "./styles";
 
-export default function Footer() {
+export default function Footer({ data }) {
+  const small = data.user.profile_image.small;
+  const large = data.user.profile_image.large;
+  const username = data.user.username;
   return (
     <View style={styles.footer}>
-      <TouchableOpacity style={styles.footerButton}>
-        <Feather name="plus-circle" size={26} color="#40494E" />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.footerButton}>
-        <Feather name="download" size={26} color="#40494E" />
-      </TouchableOpacity>
+      <View style={styles.profile}>
+        <LazyImage
+          small={small}
+          source={large}
+          style={{ width: 35, height: 35 }}
+          borderRadius={25}
+        />
+        <Text style={styles.username}>{username}</Text>
+      </View>
+      <View style={styles.buttonGroup}>
+        <TouchableOpacity style={styles.footerButton}>
+          <Feather name="plus-circle" size={26} color="#40494E" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerButton}>
+          <Feather name="download" size={26} color="#40494E" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/components/Photo/Footer/styles.js
+++ src/components/Photo/Footer/styles.js
@@ -3,10 +3,25 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   footer: {
     flexDirection: "row",
-    justifyContent: "flex-end",
+    justifyContent: "space-between",
     alignItems: "center",
     paddingHorizontal: 20,
     paddingBottom: 10,
+  },
+  profile: {
+    borderWidth: 1,
+    borderColor: "#40494E",
+    padding: 5,
+    borderRadius: 50,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  username: {
+    fontSize: 16,
+    paddingHorizontal: 8,
+  },
+  buttonGroup: {
+    flexDirection: "row",
   },
   footerButton: {
     padding: 10,

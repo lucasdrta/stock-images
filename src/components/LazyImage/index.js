e7d2176e18b7987299ea51cb06f6abb9d4dc5d14
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-25 22:27:17 -0300

    Add boderRadius

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { ImageBackground, Image } from "react-native";
 
-export default function index({ source, small, style }) {
+export default function index({ source, small, style, borderRadius }) {
   return (
     <ImageBackground
       source={{ uri: small }}
       resizeMode="cover"
       style={style}
+      borderRadius={borderRadius}
       blurRadius={3}
     >
-      <Image source={{ uri: source }} resizeMode="cover" style={style} />
+      <Image
+        source={{ uri: source }}
+        resizeMode="cover"
+        style={style}
+        borderRadius={borderRadius}
+      />
     </ImageBackground>
   );
 }

53df615a6835812ec0dc9eefa34b7363c374a2c0
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-26 12:28:59 -0300

    Add navigation to Search

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 import styles from "./styles";
 
 export default function Header() {
+  const navigation = useNavigation();
+
+  function navigateToSearch() {
+    navigation.navigate("Search");
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>ImgStock</Text>
-      <TouchableOpacity style={styles.searchButton}>
+      <TouchableOpacity style={styles.searchButton} onPress={navigateToSearch}>
         <Feather name="search" size={24} color="black" />
       </TouchableOpacity>
     </View>

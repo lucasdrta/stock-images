a8b217683b96b7a3f2a119c9d5dda3760ca3c808
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-26 11:16:54 -0300

    Add topic context functions

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,44 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { ImageContext } from "../../contexts/ImageContext";
 
 import styles from "./styles";
 export default function Categories() {
+  const { topics, loadTopicPhotos, loadApiData } = useContext(ImageContext);
   return (
-    <View>
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        style={{ backgroundColor: "#fff" }}
-        contentContainerStyle={styles.categoryContainerStyle}
-      >
-        <TouchableOpacity style={styles.categoryItem}>
-          <Text style={styles.categoryItemText}>Explore</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryItem}>
-          <Text style={styles.categoryItemText}>Fashion</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryItem}>
-          <Text style={styles.categoryItemText}>Wallpaper</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryItem}>
-          <Text style={styles.categoryItemText}>Nature</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryItem}>
-          <Text style={styles.categoryItemText}>People</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryItem}>
-          <Text style={styles.categoryItemText}>Archtecture</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryItem}>
-          <Text style={styles.categoryItemText}>Business & Work</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryItem}>
-          <Text style={styles.categoryItemText}>Film</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryItem}>
-          <Text style={styles.categoryItemText}>Travel</Text>
-        </TouchableOpacity>
-      </ScrollView>
-    </View>
+    <ImageContext.Provider>
+      <View>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{ backgroundColor: "#fff" }}
+          contentContainerStyle={styles.categoryContainerStyle}
+        >
+          <TouchableOpacity
+            style={styles.categoryItem}
+            onPress={async () => await loadApiData(1, true)}
+          >
+            <Text style={styles.categoryItemText}>Editorial</Text>
+          </TouchableOpacity>
+          {topics.map((topic) => (
+            <TouchableOpacity
+              key={topic.id}
+              style={styles.categoryItem}
+              onPress={() => loadTopicPhotos(topic.id)}
+            >
+              <Text style={styles.categoryItemText}>{topic.title}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+    </ImageContext.Provider>
   );
 }
--- src/contexts/ImageContext.js
+++ src/contexts/ImageContext.js
@@ -7,11 +7,29 @@ export function ImageProvider({ children }) {
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
+  const [topics, setTopics] = useState([]);
+
+  async function loadTopicPhotos(topic, shouldRefresh = false) {
+    setData([]);
+    fetch(
+      `https://api.unsplash.com/topics/${topic}/photos?client_id=7wQgLl4lMGkFavxqwAWHoGj2CQmlWY8JoCyhBadnxlc&per_page=20`
+    )
+      .then((response) => response.json())
+      .then((topicData) => setData(topicData));
+  }
+
+  async function loadTopicsName() {
+    fetch(
+      "https://api.unsplash.com/topics/?client_id=7wQgLl4lMGkFavxqwAWHoGj2CQmlWY8JoCyhBadnxlc&per_page=20"
+    )
+      .then((response) => response.json())
+      .then((data) => setTopics(data));
+  }
 
   async function loadApiData(pageNumber = page, shouldRefresh = false) {
     setLoading(true);
     fetch(
-      `https://api.unsplash.com/photos/?client_id=7wQgLl4lMGkFavxqwAWHoGj2CQmlWY8JoCyhBadnxlc&page=${pageNumber}`
+      `https://api.unsplash.com/photos/?client_id=7wQgLl4lMGkFavxqwAWHoGj2CQmlWY8JoCyhBadnxlc&page=${pageNumber}&per_page=50`
     )
       .then((response) => response.json())
       .then((apiData) => {
@@ -23,13 +41,14 @@ export function ImageProvider({ children }) {
 
   async function refreshiList() {
     setRefreshing(true);
-
     await loadApiData(1, true);
+
     setRefreshing(false);
   }
 
   useEffect(() => {
     loadApiData();
+    loadTopicsName();
   }, []);
   return (
     <ImageContext.Provider
@@ -39,6 +58,8 @@ export function ImageProvider({ children }) {
         loading,
         refreshiList,
         refreshing,
+        topics,
+        loadTopicPhotos,
       }}
     >
       {children}

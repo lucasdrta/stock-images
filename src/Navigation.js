4ee46477085d7066670236caba7fc2254ff6fe15
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-26 12:29:17 -0300

    Add Search screen

--- src/Navigation.js
+++ src/Navigation.js
@@ -5,12 +5,14 @@ const Stack = createStackNavigator();
 
 import Feed from "./screens/Feed";
 import Photo from "./screens/Photo";
+import Search from "./screens/Search";
 
 export default function Navigation() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Feed" component={Feed} />
       <Stack.Screen name="Photo" component={Photo} />
+      <Stack.Screen name="Search" component={Search} />
     </Stack.Navigator>
   );
 }

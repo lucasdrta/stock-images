e9a9a4efdf7c11a707529ead880fb7f7a29f430b
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-06-16 17:37:05 -0300

    Add Login Screen route

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,13 +6,15 @@ const Stack = createStackNavigator();
 import Feed from "./screens/Feed";
 import Photo from "./screens/Photo";
 import Search from "./screens/Search";
+import Login from "./screens/Login"
 
 export default function Navigation() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
       <Stack.Screen name="Feed" component={Feed} />
       <Stack.Screen name="Photo" component={Photo} />
       <Stack.Screen name="Search" component={Search} />
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 }
